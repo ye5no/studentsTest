5c78fafecfe9945385b733bf337fe744b9298a3c
Goltcer Vlad <en100500@gmail.com>
2019-09-30 12:10:23 +0500

    e2e test GraphQl (upd)

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { EditStudentDto } from '../src/dto/edit-student.dto';
 
 describe('AppController (e2e)', () => {
   let app;
+  let test: EditStudentDto;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -22,7 +24,7 @@ describe('AppController (e2e)', () => {
 
     expect(Array.isArray(response.body)).toBe(true);
 
-    const test = { ...response.body[0] };
+    test = { ...response.body[0] };
     console.log(test);
     expect(Number.isInteger(test.id)).toBe(true);
     expect(test.name !== undefined).toBe(true);
@@ -33,25 +35,28 @@ describe('AppController (e2e)', () => {
   it('/graphql', async () => {
     const response = await request(app.getHttpServer())
       .post('/graphql')
-      .send({ query: `{
-          students {
+      .set('Accept', 'application/json')
+      .send({
+        query: `query q($arg: Float!) {
+          student (id: $arg) {
             id
             name
             surname
             birth
           }
         }`,
+        operationName: 'q',
+        variables: { arg: test.id },
       })
-      .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200);
 
-    const test = response.body.data.students[0];
-    console.log(test);
-    expect(Number.isInteger(test.id)).toBe(true);
-    expect(test.name !== undefined).toBe(true);
-    expect(test.surname !== undefined).toBe(true);
-    expect(Number.isNaN(Date.parse(test.birth))).toBe(false);
+    const result = response.body.data.student;
+    console.log(result);
+    expect(result.id).toBe(test.id);
+    expect(result.name).toBe(test.name);
+    expect(result.surname).toBe(test.surname);
+    expect(result.birth).toBe(test.birth);
   });
 
   afterAll(async () => {
